26f486bf401cd90a99f755b8dc2416bd409377c9
Mazen Bondok <bondok@google.com>
2025-04-13 17:16:21 +0200

    python_operators

---
+++ python_operator_exercises.py
@@ -0,0 +1,20 @@
+# Examples of Python Operator Exercises
+# Exercise 18-a: Assignment Operator =
+lst=[
+    "yellow",
+    "white",
+    "blue"
+]
+
+print(lst)
+# End of excercise 18-a
+
+# Exercise 18-b: Division Operator /
+a=10
+b=3
+
+result= a / b
+print(result)
+# End of exercise 18-b
+
+# Exercise 18-c: Addition using +=
